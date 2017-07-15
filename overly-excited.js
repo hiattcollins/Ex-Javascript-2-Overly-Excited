81183bde889353b817e8baa2e346a3b823a699bc
Deaver Hiatt Collins <Hiatt@MacBook-Pro.local>
2017-07-15 09:57:13 -0500

    added exercise challenge loop to increase number of exclamation points

--- overly-excited.js
+++ overly-excited.js
@@ -3,21 +3,28 @@ var sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the
 
 console.log(sentence);
 
+// ********CHALLENGE function********* //
 
-
-
-// // The addExcitement function should accept the array as the sole argument
 
 function addExcitement (theWordArray) {
     	
     	let outputSentence = "";
+    	let exclamationPts = "";
 
     for (let i = 0; i < theWordArray.length; i++) {
 
     	if (i === 0) {
     		outputSentence = theWordArray[i];
     	} else if ( ([i] + 1) % 3 === 0 ) {
-    		outputSentence = outputSentence + " " + theWordArray[i] + "!";
+
+    		exclamationPts = "";
+
+    		for (let j = 0; j < (i / 3); j++) {
+    			exclamationPts = exclamationPts + "!";
+    		}
+
+    		outputSentence = outputSentence + " " + theWordArray[i] + exclamationPts;
+
     	} else {
     		outputSentence = outputSentence + " " + theWordArray[i];
     	}
@@ -29,6 +36,33 @@ function addExcitement (theWordArray) {
 }
 
 addExcitement(sentence);
+
+
+// ****** Exercise Function ********** //
+
+// function addExcitement (theWordArray) {
+    	
+//     	let outputSentence = "";
+
+//     for (let i = 0; i < theWordArray.length; i++) {
+
+//     	if (i === 0) {
+//     		outputSentence = theWordArray[i];
+//     	} else if ( ([i] + 1) % 3 === 0 ) {
+//     		outputSentence = outputSentence + " " + theWordArray[i] + "!";
+//     	} else {
+//     		outputSentence = outputSentence + " " + theWordArray[i];
+//     	}
+
+//     	console.log(outputSentence);
+
+//     }
+
+// }
+
+// addExcitement(sentence);
+
+
 
 
 //       Write a `for` loop that iterates over the array argument and
